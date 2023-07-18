51d6d091a7d2b4d6770412a4cddc3ee0fd39a3f9
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-07-18 16:36:41 +0530

    Update Orders.js

--- Component/Orders.js
+++ Component/Orders.js
@@ -42,19 +42,25 @@ const Orders=(props)=>{
 
     },[props.pass.table])
 
-    const T1DeleteHandler=()=>{
-        localStorage.removeItem(props.pass.id)
-        setTable1([])   
+    const T1DeleteHandler=(key)=>{
+        localStorage.removeItem(key)
+        const index= Table1.findIndex(key)
+        Table1.splice(index,1)
+        setTable1([...Table1])   
     }
 
-    const T2DeleteHandler=()=>{
-        localStorage.removeItem(props.pass.id)
-        setTable2([])   
+    const T2DeleteHandler=(key)=>{
+        localStorage.removeItem(key)
+        const index= Table2.findIndex(key)
+        Table2.splice(index,1)
+        setTable2([...Table2])   
     }
 
-    const T3DeleteHandler=()=>{
-        localStorage.removeItem(props.pass.id)
-        setTable3([])   
+    const T3DeleteHandler=(key)=>{
+        localStorage.removeItem(key)
+        const index= Table3.findIndex(key)
+        Table3.splice(index,1)
+        setTable3([...Table3])   
     }
     
    
@@ -63,11 +69,11 @@ const Orders=(props)=>{
         <React.Fragment>
             <h2>Orders</h2>
             <div id="Table-1">Table 1</div>
-            {Table1.map((i)=>(<li key={obj.id}>{i.name}-{i.price}-{i.table}<button onClick={T1DeleteHandler}>Delete</button></li>)) }
+            {Table1.map((i)=>(<li key={obj.id}>{i.name}-{i.price}-{i.table}<button onClick={T1DeleteHandler(event,key)}>Delete</button></li>)) }
             <div id="Table-2">Table 2</div>
-            {Table2.map((i)=>(<li key={obj.id}>{i.name}-{i.price}-{i.table}<button onClick={T2DeleteHandler}>Delete</button></li>)) }
+            {Table2.map((i)=>(<li key={obj.id}>{i.name}-{i.price}-{i.table}<button onClick={T2DeleteHandler(event,key)}>Delete</button></li>)) }
             <div id="Table-3">Table 3</div>
-            {Table3.map((i)=>(<li key={obj.id}>{i.name}-{i.price}-{i.table}<button onClick={T3DeleteHandler}>Delete</button></li>)) }
+            {Table3.map((i)=>(<li key={obj.id}>{i.name}-{i.price}-{i.table}<button onClick={T3DeleteHandler(event,key)}>Delete</button></li>)) }
 
         </React.Fragment>
     )
